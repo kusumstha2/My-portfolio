5e0db190f521593ace8fd3a25b1da561ff8a6fd8
kusum <kusumstha251@gmail.com>
2025-05-14 14:52:53 +0545

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,7 +127,7 @@ function showProjects(projects) {
     </div>`
     :projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
